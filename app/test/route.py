0daab8e92ddeb7d4fd94171fd8fcbda1ec8c6747
Daniel Mikes <max8@post.cz>
2024-12-08 23:19:09 +0100

    Remove tst.route

--- app/test/route.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from ..util.logger import log
-
-tst = Blueprint('tst', __name__, url_prefix='/test', static_folder='.', template_folder='.')
-
-@tst.route('/')
-def test():
-  return 'test'
